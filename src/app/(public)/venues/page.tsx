2094a7432854d117707860c9a093f6f07f28c623
MathewCobley <mathewcobley1@gmail.com>
2026-05-09 00:26:50 +0100

    Prevent venues page prerender database error

--- src/app/(public)/venues/page.tsx
+++ src/app/(public)/venues/page.tsx
@@ -5,6 +5,9 @@
 import Link from "next/link";
 import { prisma } from "@/lib/prisma";
 
+export const dynamic = "force-dynamic";
+export const revalidate = 0;
+
 type VenueCardData = {
   name: string;
   league: string;
