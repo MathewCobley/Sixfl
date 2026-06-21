d61921c843eb0bc8c6e19561a6a29de2d7b374d7
MathewCobley <mathewcobley1@gmail.com>
2026-06-22 00:09:17 +0100

    Unassign prospects when marking duplicate

--- src/app/api/admin/player-prospects/[prospectId]/duplicate/route.ts
+++ src/app/api/admin/player-prospects/[prospectId]/duplicate/route.ts
@@ -38,6 +38,7 @@ export async function POST(
     await prisma.teamPlayerProspect.update({
       where: { id: prospect.id },
       data: {
+        teamId: null,
         status: "DUPLICATE",
       },
     });
