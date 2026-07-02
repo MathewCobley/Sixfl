418707ae7b351a8f0fcf197e0b0f0352f6e63efd
MathewCobley <mathewcobley1@gmail.com>
2026-07-02 17:42:26 +0100

    Use competition picker on team pages

--- src/app/(admin)/admin/layout.tsx
+++ src/app/(admin)/admin/layout.tsx
@@ -24,8 +24,7 @@ import RefereeNightFixtureSyncBridge from "@/components/admin/referee-nights/Ref
 import AdminRefereeCommsHistoryBridge from "@/components/admin/referees/AdminRefereeCommsHistoryBridge";
 import RefereeWelcomeInviteBridge from "@/components/admin/referees/RefereeWelcomeInviteBridge";
 import AdminSocialResultsGeneratorLinksBridge from "@/components/admin/social/AdminSocialResultsGeneratorLinksBridge";
-import AdminCurrentLeagueSelectBridge from "@/components/admin/teams/AdminCurrentLeagueSelectBridge";
-import TeamDivisionPickerBridge from "@/components/admin/teams/TeamDivisionPickerBridge";
+import TeamCompetitionPickerBridge from "@/components/admin/teams/TeamCompetitionPickerBridge";
 import TeamReplaceFixturesButtonBridge from "@/components/admin/teams/TeamReplaceFixturesButtonBridge";
 import AppHeader from "@/components/layout/AppHeader";
 
@@ -58,8 +57,7 @@ export default async function AdminLayout({
       <AdminSidebarDesktopColumnsBridge />
       <AdminLeagueSeasonsBridge />
       <AdminDivisionSelectBridge />
-      <AdminCurrentLeagueSelectBridge />
-      <TeamDivisionPickerBridge />
+      <TeamCompetitionPickerBridge />
       <TeamReplaceFixturesButtonBridge />
       <RefereeNightFixtureSyncBridge />
       <RefereeNightCashDistributionBridge />
