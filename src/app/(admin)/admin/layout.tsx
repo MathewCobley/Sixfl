bed53cac4b2ac021b29b7014b86492f5737d3fe5
MathewCobley <mathewcobley1@gmail.com>
2026-07-02 17:15:10 +0100

    Enable current-season league filtering on team pages

--- src/app/(admin)/admin/layout.tsx
+++ src/app/(admin)/admin/layout.tsx
@@ -24,6 +24,7 @@ import RefereeNightFixtureSyncBridge from "@/components/admin/referee-nights/Ref
 import AdminRefereeCommsHistoryBridge from "@/components/admin/referees/AdminRefereeCommsHistoryBridge";
 import RefereeWelcomeInviteBridge from "@/components/admin/referees/RefereeWelcomeInviteBridge";
 import AdminSocialResultsGeneratorLinksBridge from "@/components/admin/social/AdminSocialResultsGeneratorLinksBridge";
+import AdminCurrentLeagueSelectBridge from "@/components/admin/teams/AdminCurrentLeagueSelectBridge";
 import TeamDivisionPickerBridge from "@/components/admin/teams/TeamDivisionPickerBridge";
 import TeamReplaceFixturesButtonBridge from "@/components/admin/teams/TeamReplaceFixturesButtonBridge";
 import AppHeader from "@/components/layout/AppHeader";
@@ -57,6 +58,7 @@ export default async function AdminLayout({
       <AdminSidebarDesktopColumnsBridge />
       <AdminLeagueSeasonsBridge />
       <AdminDivisionSelectBridge />
+      <AdminCurrentLeagueSelectBridge />
       <TeamDivisionPickerBridge />
       <TeamReplaceFixturesButtonBridge />
       <RefereeNightFixtureSyncBridge />
