fd535377b843e8d53f9d8def8bab2ad5ebde62da
MathewCobley <mathewcobley1@gmail.com>
2026-07-19 19:39:48 +0100

    Remove DOM kit bridges

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -3,15 +3,6 @@
 
 import { SessionProvider } from "next-auth/react";
 
-import TeamKitColourAdminBridge from "@/components/admin/teams/TeamKitColourAdminBridge";
-import FixtureKitShirtsBridge from "@/components/fixtures/FixtureKitShirtsBridge";
-
 export default function Providers({ children }: { children: React.ReactNode }) {
-  return (
-    <SessionProvider>
-      <TeamKitColourAdminBridge />
-      <FixtureKitShirtsBridge />
-      {children}
-    </SessionProvider>
-  );
+  return <SessionProvider>{children}</SessionProvider>;
 }
