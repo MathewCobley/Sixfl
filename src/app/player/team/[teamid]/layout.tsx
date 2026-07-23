268eedc81cf1f4aa8744b1f8450d5f4fcad28018
MathewCobley <mathewcobley1@gmail.com>
2026-07-23 14:18:10 +0100

    Tighten player dashboard spacing

--- src/app/player/team/[teamid]/layout.tsx
+++ src/app/player/team/[teamid]/layout.tsx
@@ -19,16 +19,24 @@ export default async function PlayerTeamLayout({
   const { teamid } = await params;
 
   return (
-    <>
+    <div className="player-team-layout min-h-screen bg-[#07130f]">
+      <style>{`
+        .player-team-layout > main {
+          min-height: auto !important;
+          padding-bottom: 1rem !important;
+        }
+      `}</style>
       <Suspense>
         <PlayerPreviewLinkPersistence teamId={teamid} />
       </Suspense>
       {children}
       <PlayerDashboardOnly teamId={teamid}>
-        <PlayerLeagueMediaPanel teamId={teamid} />
+        <div className="space-y-8 pb-8">
+          <PlayerLeagueMediaPanel teamId={teamid} />
+          <PlayerPerformancePanel teamId={teamid} />
+          <PlayerMessageBox teamId={teamid} />
+        </div>
       </PlayerDashboardOnly>
-      <PlayerPerformancePanel teamId={teamid} />
-      <PlayerMessageBox teamId={teamid} />
-    </>
+    </div>
   );
 }
