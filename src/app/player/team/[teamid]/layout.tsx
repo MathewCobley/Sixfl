bb2e2b42752313c81c1cb7bd49a0b5cc5c47265e
MathewCobley <mathewcobley1@gmail.com>
2026-07-23 13:28:45 +0100

    Show league results TV and predictor on player dashboard

--- src/app/player/team/[teamid]/layout.tsx
+++ src/app/player/team/[teamid]/layout.tsx
@@ -3,6 +3,7 @@
 // ========================================
 
 import { Suspense, type ReactNode } from "react";
+import PlayerLeagueMediaPanel from "@/components/player/PlayerLeagueMediaPanel";
 import PlayerMessageBox from "@/components/player/PlayerMessageBox";
 import PlayerPerformancePanel from "@/components/player/PlayerPerformancePanel";
 import PlayerPreviewLinkPersistence from "@/components/player/PlayerPreviewLinkPersistence";
@@ -22,6 +23,7 @@ export default async function PlayerTeamLayout({
         <PlayerPreviewLinkPersistence teamId={teamid} />
       </Suspense>
       {children}
+      <PlayerLeagueMediaPanel teamId={teamid} />
       <PlayerPerformancePanel teamId={teamid} />
       <PlayerMessageBox teamId={teamid} />
     </>
