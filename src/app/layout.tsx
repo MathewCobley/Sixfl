a95e16297af67054a25593bc59321a1075cfa4f0
MathewCobley <mathewcobley1@gmail.com>
2026-08-03 23:33:24 +0100

    Remove replaced captain DOM bridges from root layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,9 +14,7 @@ import NightBoardWarningsPositionBridge from "@/components/admin/night-board/Nig
 import AdminPaymentsPageBridge from "@/components/admin/payments/AdminPaymentsPageBridge";
 import AdminPlayerPoolNavBridge from "@/components/admin/player-pool/AdminPlayerPoolNavBridge";
 import CaptainAdditionalCaptainBridge from "@/components/captain/CaptainAdditionalCaptainBridge";
-import CaptainFixturesDeduplicateBridge from "@/components/captain/CaptainFixturesDeduplicateBridge";
 import CaptainHeaderLeaguePositionBridge from "@/components/captain/CaptainHeaderLeaguePositionBridge";
-import CaptainPlayerPoolNavBridge from "@/components/captain/CaptainPlayerPoolNavBridge";
 import HideImpossibleLeaguePositionBridge from "@/components/captain/HideImpossibleLeaguePositionBridge";
 import TeamAutoPayCopyBridge from "@/components/captain/TeamAutoPayCopyBridge";
 import NorthallertonWaitingListCopyBridge from "@/components/public/NorthallertonWaitingListCopyBridge";
@@ -120,9 +118,7 @@ export default function RootLayout({
             <AdminPaymentsPageBridge />
             <AdminPlayerPoolNavBridge />
             <CaptainAdditionalCaptainBridge />
-            <CaptainFixturesDeduplicateBridge />
             <CaptainHeaderLeaguePositionBridge />
-            <CaptainPlayerPoolNavBridge />
             <HideImpossibleLeaguePositionBridge />
             <TeamAutoPayCopyBridge />
             <NorthallertonWaitingListCopyBridge />
