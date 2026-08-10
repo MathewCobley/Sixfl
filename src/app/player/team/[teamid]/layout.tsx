350f56f8f24d387305ce4535f93433556346f266
MathewCobley <mathewcobley1@gmail.com>
2026-08-10 01:22:39 +0100

    Keep temporary fees in unified player ledger

--- src/app/player/team/[teamid]/layout.tsx
+++ src/app/player/team/[teamid]/layout.tsx
@@ -9,7 +9,6 @@ import PlayerLeagueMediaPanel from "@/components/player/PlayerLeagueMediaPanel";
 import PlayerMessageBox from "@/components/player/PlayerMessageBox";
 import PlayerPreviewLinkPersistence from "@/components/player/PlayerPreviewLinkPersistence";
 import PlayerTeamNav from "@/components/player/PlayerTeamNav";
-import PlayerTemporaryMatchFeesPanel from "@/components/player/PlayerTemporaryMatchFeesPanel";
 
 export default async function PlayerTeamLayout({
   children,
@@ -38,9 +37,6 @@ export default async function PlayerTeamLayout({
             teamId={teamid}
             href={`/goal-of-the-week?from=player&teamId=${encodeURIComponent(teamid)}`}
           />
-          <Suspense fallback={null}>
-            <PlayerTemporaryMatchFeesPanel teamId={teamid} />
-          </Suspense>
         </div>
       </PlayerDashboardOnly>
       {children}
